7a30f172af468949554e55cfc85d6b90af463a66
sebek556 <59932275+sebek556@users.noreply.github.com>
2020-01-22 19:59:50 +0100

    zadanie2

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ require (["esri/Map", "esri/views/MapView"],
    function(Map, MapView)
    {
       let map1=new Map({basemap:"topo"})
+      let map2=new Map({basemap:"satellite"})
+      let map3=new Map({basemap:"osm"})
       let mapContainer=new MapView({
-          container:"map1",
+          container:"m1",
           map:map1
+          
           
 
 
       });
+      
+ 
    })
