f02db3524338b0f4067d8dc1ed087b6f421eed3d
sebek556 <59932275+sebek556@users.noreply.github.com>
2020-01-30 23:33:59 +0100

    koniec_v1

--- script.js
+++ script.js
@@ -1,18 +1,98 @@
+mapa1 = document.getElementById("mapa1");
+mapa2 = document.getElementById("mapa2");
+mapa3 = document.getElementById("mapa3");
+mapa4 = document.getElementById("mapa4");
+
+przycisk1 = document.getElementById("przycisk1");
+przycisk2 = document.getElementById("przycisk1");
+przycisk3 = document.getElementById("przycisk1");
+przycisk4 = document.getElementById("przycisk1");
 
 require (["esri/Map", "esri/views/MapView"],
    function(Map, MapView)
    {
-      let map1=new Map({basemap:"topo"})
-      let map2=new Map({basemap:"satellite"})
-      let map3=new Map({basemap:"osm"})
-      let mapContainer=new MapView({
-          container:"m1",
-          map:map1
-          
-          
+    let map1 = new Map({basemap:"streets"});
 
+    let mapContainer = new MapView({
+        container: "d",  
+        map: map1  
+        , zoom:14,
+        center: [22.57, 51.25],              
+     }); 
 
-      });
-      
- 
    })
+
+require (["esri/Map", "esri/views/MapView"],
+   function(Map, MapView)
+   {
+   mapa1.addEventListener("click", function(){
+    let map1 = new Map({basemap:"satellite"});
+
+    let mapContainer = new MapView({
+        container: "d",  
+        map: map1  
+        , zoom:14,
+        center: [22.57, 51.25],       
+     }); 
+
+   })})
+
+   require (["esri/Map", "esri/views/MapView"],
+   function(Map, MapView)
+   {
+   mapa2.addEventListener("click", function(){
+    let map1 = new Map({basemap:"topo"});
+
+    let mapContainer = new MapView({
+        container: "d",  
+        map: map1,
+        zoom:14,
+        center: [22.57, 51.25],     
+     }); 
+
+   })})
+
+   require (["esri/Map", "esri/views/MapView"],
+   function(Map, MapView)
+   {
+   mapa3.addEventListener("click", function(){
+    let map1 = new Map({basemap:"osm"});
+
+    let mapContainer = new MapView({
+        container: "d",  
+        map: map1  
+        , zoom:14,
+        center: [22.57, 51.25],   
+     }); 
+
+   })})
+
+   require (["esri/Map", "esri/views/MapView", "esri/WebMap"],
+   function(Map, MapView, WebMap, webMercatorUtils)
+   {
+   mapa4.addEventListener("click", function(){
+    let map1 = new WebMap({
+      portalItem: {
+         id: "ef139263371049d99aec9894f1c09258"
+      }
+   })
+
+    let mapContainer = new MapView({
+        container: "d",   
+        map: map1  
+        , zoom:14,
+        center: [22.57, 51.25],    
+     }); 
+
+     var basemapGallery = new BasemapGallery({
+      view: view,
+      source: {
+        portal: {
+          url: "https://www.arcgis.com",
+          useVectorBasemaps: true  // Load vector tile basemaps
+        }
+      }
+      
+    });
+    view.ui.add(basemapGallery, "top-right");
+   })})
